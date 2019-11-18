b4f5796a07d4750daf01462d90fdd33c630e61a2
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-18 21:50:17 +0100

    xb

---
+++ Open-Lab-04.01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+        public static bool DoubleLetters(string str)
+        {
+            char[] word = str.ToCharArray();
+            foreach (var c in word)
+            {
+                for (int a = 0; a == 2;)
+                {
+                    if (c == word[a])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string w = Console.ReadLine();
+            Console.WriteLine(DoubleLetters(w));
+        }
+    }
+}
